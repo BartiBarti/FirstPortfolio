e3aaba3819a78ee81be5e566b0196841108b1910
Bart <bakosberg@gmail.com>
2021-02-07 22:51:39 +0100

    Add funct weekday date

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-console.log("RADEK")
+var weekdaySpan = document.getElementById("weekday");
+var dateSpan = document.getElementById("date");
 
+var currentWeekDay = "";
+
+function getCurrentWeekDay() { 
+    var result;
+    var daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var currentDate = new Date();
+    var currentDay = currentDate.getDay();
+    result = daysArray[currentDay];
+    return result;
+}
+currentWeekDay = getCurrentWeekDay();
+weekdaySpan.innerText = currentWeekDay;
+
+var optionsForDate = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
+var dateToShow = new Date().toLocaleDateString('PL', optionsForDate);
+dateSpan.innerText = dateToShow;
